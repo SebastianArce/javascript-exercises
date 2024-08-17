2551ed7fa4046ced14ca14743926021b48d7b863
Sebastian Arce <sebastianarce25@gmail.com>
2024-08-17 14:42:24 +0100

    solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i=1; i < arguments.length; i++) {
+        let valueToDrop = arguments[i]
+        array = array.filter(a => a !== valueToDrop)
+    }
+    // console.log(arguments)
+    // console.log(array)
+    return array
 };
 
 // Do not edit below this line
