757a167610c63d953176c8ae9e90e6a8d58685ea
Sebastian Arce <sebastianarce25@gmail.com>
2024-09-02 10:14:53 +0100

    solve 08_calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(...args) {
+  const initialValue = 0;
+  return args.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+  )
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	const initialValue = 0;
+  return args.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+  )
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  const initialValue = 1;
+  return args.reduce(
+    (accumulator, currentValue) => accumulator * currentValue,
+    initialValue
+  )
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 0;
+  if (num === 0) {
+    return 1
+  }
+  while (num > 0) {
+    if (factorial == 0) {
+      factorial = num
+    } else {
+      factorial *= num
+    }
+    num -= 1
+  }
+  return factorial
 };
 
 // Do not edit below this line
