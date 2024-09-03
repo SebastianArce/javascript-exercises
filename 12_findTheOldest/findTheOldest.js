3464c75c2f624451f264fe3b3f2fb2387b8b0075
Sebastian Arce <sebastianarce25@gmail.com>
2024-09-03 11:16:24 +0100

    solve 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let peopleSorted = people.sort((a, b) => {
+        let currentYear = new Date().getFullYear()
+        if (! ("yearOfDeath" in a)) {
+            a["yearOfDeath"] = currentYear;
+        }
+        if (! ("yearOfDeath" in b)) {
+            b["yearOfDeath"] = currentYear;
+        }
 
+        age_a = a.yearOfDeath - a.yearOfBirth
+        age_b = b.yearOfDeath - b.yearOfBirth
+
+        if (age_a < age_b) {
+            return 1
+        } else {
+            return -1
+        }
+    })
+    return peopleSorted[0]
 };
 
 // Do not edit below this line
