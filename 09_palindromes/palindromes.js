645ec7b998368f09ec7827abdf8573da8446e667
Sebastian Arce <sebastianarce25@gmail.com>
2024-09-03 08:04:03 +0100

    solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanString = string.split(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g).join('')
+    cleanString = cleanString.toLowerCase()
+    if (cleanString.split('').reverse().join('') == cleanString) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
